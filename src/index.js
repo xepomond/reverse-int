9ba35f09a1ee74d31ff778c855cbc7b875af5e5c
xepomond <anka4717@gmail.com>
2022-04-04 23:42:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+   let result = '';
+   if (n < 0){
+      n = n + '';
+      result = (n.split('').reverse().join(''));
+      let lastSymbol = result[result.length - 1];
+      return ( result.split(lastSymbol).join(''));
+      }
+   else {
+      n = n + ""; 
+      return (n.split('').reverse().join(''));
+   }
 }
